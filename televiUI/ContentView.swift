e1ef43d5ca695d7c5a02fe7e2aec505d56f1234a
Henrique Lima <henriquelima@tokenlab.com.br>
2020-01-08 10:38:44 -0300

    Initial Commit

---
+++ televiUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  televiUI
+//
+//  Created by HenriqueLima on 08/01/20.
+//  Copyright © 2020 HenriqueLima. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
